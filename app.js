25fb512dd84cde27102ec689e3c62028c714e96f
Bea Avis <bea.avis.p@gmail.com>
2021-04-17 12:05:39 +0200

    Update teams route and link

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // Set the view engine to render views & the path to the views
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 // Set the path to public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Generate routes
-app.get('/', (req, res, next) => res.render('index'));
+app.get("/", (req, res, next) => res.render("index"));
 
-app.get('/players', (req, res, next) => res.render('players'));
+app.get("/players", (req, res, next) => res.render("players"));
 
-app.get('/teams', (req, res, next) => res.render('teams'));
+app.get("/teams", (req, res, next) => {
+  // To skip the layout on this route
+  const data = {
+    layout: false,
+  };
+  res.render("teams", data);
+});
 
 // Listen to the server in port 3000
-app.listen(3000, () => console.log('Connected to the server in port 3000'));
+app.listen(3000, () => console.log("Connected to the server in port 3000"));
